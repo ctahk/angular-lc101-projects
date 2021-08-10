db6a88b38bccfc82b0e03c2cc8683d0128277155
Christi <christi.tahk@gmail.com>
2021-08-10 17:09:19 -0500

    Finished Part 1 of Exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  photosTitle = 'Specific Images';
+  image1 = 'https://i2.wp.com/smittenkitchen.com/wp-content/uploads/2021/07/frozen-strawberry-daiquiris-scaled.jpg?w=2560&ssl=1';
+  image2 = 'https://i1.wp.com/live.staticflickr.com/65535/51365382081_716f0a64e3_z.jpg?resize=640%2C427&ssl=1';
   image3 = '';
 
   constructor() { }
