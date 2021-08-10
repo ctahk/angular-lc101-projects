e731e7a92d6ae0f53ee8b5eddd9de5fc557ac34f
Christi <christi.tahk@gmail.com>
2021-08-10 17:47:52 -0500

    Finished Exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Specific Images';
-  image1 = 'https://i2.wp.com/smittenkitchen.com/wp-content/uploads/2021/07/frozen-strawberry-daiquiris-scaled.jpg?w=2560&ssl=1';
-  image2 = 'https://i1.wp.com/live.staticflickr.com/65535/51365382081_716f0a64e3_z.jpg?resize=640%2C427&ssl=1';
-  image3 = '';
+  photosTitle = 'SK Food Images';
+  image1 = 'https://i2.wp.com/farm5.staticflickr.com/4905/44584674540_84d34f5bf5_z.jpg?resize=640%2C427&ssl=1';
+  image2 = 'https://i2.wp.com/smittenkitchen.com/wp-content/uploads/2021/07/frozen-strawberry-daiquiris-scaled.jpg?w=2560&ssl=1';
+  image3 = 'https://i1.wp.com/live.staticflickr.com/65535/51365382081_716f0a64e3_z.jpg?resize=640%2C427&ssl=1';
 
   constructor() { }
 
